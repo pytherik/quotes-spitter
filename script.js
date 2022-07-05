171f5d0821cc0fadaf84f9f02dfbc8954fbaeba2
pytherik <erikberndt@gmx.net>
2022-07-05 15:22:50 +0200

    twitter Button working

--- script.js
+++ script.js
@@ -21,11 +21,25 @@ const newQuote = () => {
     getQuote().then(data => {
     if (!data.author) {
       data.author = "unbekannt";
-    }  
+    }
+    if (data.text.length > 100) {
+      quoteText.classList.add('long-quote');
+    } else {
+      quoteText.classList.remove('long-quote');
+    }
     quoteText.textContent = data.text;
     authorText.textContent = data.author;
   });
 }
+
+function tweetQuote() {
+  const twitterURL = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterURL, '_blank');
+}
+
+buttonTwitter.addEventListener("click", () => {
+  tweetQuote();
+});
 
 buttonNew.addEventListener("click", () => {
     newQuote();
